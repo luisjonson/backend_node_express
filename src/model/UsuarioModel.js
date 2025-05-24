e16e51dcce73a9df8cb26efaa299514b12d45e0a
Jonson <luizlindonjonson@yahoo.com.br>
2025-05-24 18:56:02 -0300

    Removendo o comentario  do usuario model.

--- src/model/UsuarioModel.js
+++ src/model/UsuarioModel.js
@@ -36,45 +36,4 @@ UsuarioModel.init({
     sequelize: connection,
 });
 
-
-// class UsuarioModel {
-
-//     static lista = [
-//         {
-//         nome: "admin",
-//         Login: "admin",
-//         senha: "123"
-//         }
-//     ];
-
-//     static login(login, senha) {
-//        const indice = UsuarioModel.lista.findIndex(item => item.Login == login && item.senha == senha);
-//        return UsuarioModel.lista[indice];
-//     }
-//     static listar() {
-//         return UsuarioModel.lista;
-//     }
-
-//     static consultarPorId(id) {
-//         const dados = UsuarioModel.lista.filter(item => item.id === id);
-//         return dados;
-
-//     }
-
-//     static criar(data) {
-//         UsuarioModel.lista.push(data)
-//     }
-
-//     static atualizar(id, data) {
-
-//         const indice = UsuarioModel.lista.findIndex(item => item.id == id);
-//         UsuarioModel.lista[indice] = data;
-//     }
-
-//     static deletar(id) {
-//          const dados = UsuarioModel.lista.filter(item => item.id != id);
-//          return UsuarioModel.lista = dados;
-//     }
-// }
-
 export default UsuarioModel;
