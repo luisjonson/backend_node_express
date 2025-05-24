671efbce812d2ad56e28b4427a7fad79cc0e54bd
Jonson <luizlindonjonson@yahoo.com.br>
2025-05-24 14:47:54 -0300

    Renomeando a sequilze.

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 import 'dotenv/config';
-import { Sequelize } from 'sequelize';
+import { Sequelize} from 'sequelize';
 
 // Configuração da conexão com o banco de dados
-const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_SENHA, {
-  host: process.env.DATABASE_HOST,
-  dialect: 'mysql',
-  port: process.env.DATABASE_PORT
+const connection = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_SENHA, {
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql',
+    port: process.env.DATABASE_PORT,
+    logging: false
 });
 
-console.log(sequelize)
-export default sequelize;
+export default connection;
