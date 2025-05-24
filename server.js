de03757d4050dfa86cfa2f49528ceec2afe44754
Jonson <luizlindonjonson@yahoo.com.br>
2025-05-24 14:34:50 -0300

    inserindo o cors no services

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import RotasPrivadas from './src/routes/RotasPrivadas.js';
 import RotasPublicas from './src/routes/RotasPublicas.js';
+import cors from 'cors';
 
 // const host = "localhost"
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(cors());  // ← habilita CORS para todas as origens
 //Faz com que o express aceite as requisições do json.
 app.use(express.json());
 
