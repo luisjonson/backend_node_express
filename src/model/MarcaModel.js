70c6ba3402bf28778442d080c5c907013f505833
Jonson <luizlindonjonson@yahoo.com.br>
2025-06-07 11:39:40 -0300

    criando o modelo Marca.

---
+++ src/model/MarcaModel.js
@@ -0,0 +1,34 @@
+import { DataTypes, Model } from "sequelize";
+import connection from "../config/connection.js";
+
+class MarcaModel extends Model { }
+
+
+MarcaModel.init({
+    numsequencial: {
+        field: 'marnumsequencial',
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome: {
+        field: 'marnome',
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        unique:true
+    },
+    descricao: {
+        field: 'mardescricao',
+        type: DataTypes.STRING,
+    },
+    ativo: {
+        field: 'marativo',
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'tbmarca',
+    sequelize: connection,
+});
+
+export default MarcaModel;
